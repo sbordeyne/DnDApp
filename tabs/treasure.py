4ae2d03a13a287bb6ec61c66d6a1f9509c44b173
Russell Loewe <russloewe@gmail.com>
2015-08-02 15:35:28 -0700

    added doc strings

--- tabs/treasure.py
+++ tabs/treasure.py
@@ -8,10 +8,16 @@ from tkinter import Frame, Button
 
 
 class TreasureTab(Frame):
+    '''
+    Tab object for the Treasure tab.
+    '''
     def __init__(self, master=None):
         Frame.__init__(self, master)
-        button_treasure = Button(self, text="Treasure",  command=self.treasure)
+        button_treasure = Button(self, text="Treasure", command=self.treasure)
         button_treasure.grid(row=0, column=0)
 
     def treasure(self):
+        '''
+        Print 'treasure' to console when the treasure button is pressed.
+        '''
         print("treasure")
