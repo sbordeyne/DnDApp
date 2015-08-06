aedadda9f44022fc796f56abe5080a523336b47d
Russell Loewe <russloewe@gmail.com>
2015-08-05 20:39:36 -0700

    made client into object. Added fix for closing input_queue

--- networking/udp_client.py
+++ networking/udp_client.py
@@ -8,22 +8,27 @@ class client_thread(threading.Thread):
     def __init__(self, host, port, in_queue, out_queue):
         threading.Thread.__init__(self) 
         # Create a UDP socket
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         server_address = (host, port)
         self.input_queue = in_queue
         self.out_queue = out_queue
         self.running = True
         
     def send_ping(self):
+        '''
+        Send a special message "ping" to the host and listen
+        for a reply. Later this will be used to verify that the host 
+        is still active be ping once every several seconds.
+        '''
         message = 'ping'.encode()
         try:
             # Send data
             print ('sending {}'.format(message))
-            sent = sock.sendto(message, server_address)
+            sent = self.sock.sendto(message, server_address)
 
             # Receive response
             print ('waiting to receive')
-            data, server = sock.recvfrom(4096)
+            data, server = self.sock.recvfrom(4096)
             print ('received {}'.format(data))
             
         except Exception:
@@ -34,6 +39,14 @@ class client_thread(threading.Thread):
         Run in a loop parsing data from input queue.
         Special input will responf with test, or kill
         the thread.
+        
+        The test response it to let the unittest know that 
+        the in/out queues are running. The kill test is to teminate
+        the thread.
+        
+        .. note:: It's good practice to use thread.join() after sending 
+        the kill signal to prevent rogue threads.
+        
         '''
         while self.running:
             try:
@@ -42,9 +55,19 @@ class client_thread(threading.Thread):
                     self.out_queue.put(data)
                 if data == 'et tu, Brute?':
                     self.running = False
+                else:
+                    pass # Here is where I will later invoke the sending function
+
+                # after parsing through the options mark queue task done
+                self.input_queue.task_done()
+
+            # I want this client thread to run continiously untill the kill
+            # signal is sent or the host disconnects. So on an empty input 
+            # queue we will just chill out. 
             except queue.Empty:
                 pass
+        self.close()     # close the socket after exiting the loop
 
     def close(self):
         print ('closing socket')
-        sock.close()
+        self.sock.close()
