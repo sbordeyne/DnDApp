f47851fa1ca4789819bb6bc757d99b2d7191d2a8
Russell Loewe <russloewe@gmail.com>
2015-08-06 20:06:24 -0700

    added method and qaction trigger for launching the connect window from the menu

--- launch.py
+++ launch.py
@@ -3,6 +3,8 @@ import sys
 from lib import resources_rc
 from lib.main_window_ui import Ui_MainWindow
 from lib.host_window_ui import Ui_host_window
+from lib.connect_window_ui import Ui_connect_window
+
 
 try:
     _fromUtf8 = QtCore.QString.fromUtf8
@@ -21,12 +23,13 @@ except AttributeError:
 class main_window(Ui_MainWindow):
     '''
     This class inherits the Ui_Mainwindow class. The inheritance is 
-    used to add bindings to launch the host window dialog
+    used to add bindings to launch the host/connect window dialog
     '''
     def __init__(self, MainWindow):
         self.setupUi(MainWindow)
         self.actionHost_Game.triggered.connect(self.launch_host_game)
-        
+        self.actionConnect_to.triggered.connect(self.launch_connect_window)
+
     def launch_host_game(self):
         '''
         This method will launch the host window as a dialog.
@@ -35,6 +38,15 @@ class main_window(Ui_MainWindow):
         ui = Ui_host_window()
         ui.setupUi(host_dialog)
         host_dialog.exec_()
+
+    def launch_connect_window(self):
+        '''
+        This method will launch the connect window dialog
+        '''
+        connect_dialog = QtGui.QDialog()
+        ui = Ui_connect_window()
+        ui.setupUi(connect_dialog)
+        connect_dialog.exec_()
 
         
 app = QtGui.QApplication(sys.argv)
