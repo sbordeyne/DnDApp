196c10497ad48784a79342484640ea04d146e465
Dogeek <simon.bordeyne@gmail.com>
2015-08-09 09:00:11 +0200

    added functions to handle the random encounter generation

--- lib/functions.py
+++ lib/functions.py
@@ -6,7 +6,7 @@ Created on Sat Apr  4 10:21:25 2015
 """
 from time import strftime
 import sys
-
+import random as rd
 
 def read_config(file_name):
     """Function that reads the config of given filename, returns dict"""
@@ -55,35 +55,91 @@ def log(message, first_time=False):
             print("\n[{0}] {1}".format(strftime("%H:%M:%S"), message))
 
 def get_monster_dict(environment_of_encounter):
-        """
-        Method to return a dict of all the available monsters. Reads the resource/cfg/monsters.cfg file to fill the dict.
-        
-        Formatted entry in monsters.cfg:
-        monster_name;environment_name{
-        life\n
-        ac\n
-        movement\n
-        attacks\n
-        damages\n
-        number_met\n
-        saves\n
-        moral\n
-        treasure\n
-        alignment\n
-        xp_value\n
-        }
-        """
-        return_dict=dict()
-        key={}
-        key["name"] = ""
-        key["environment"] = ""
-        value={}        
-        
-        stats=["life", "ac", "movement", "attacks", "damages", "number_met", "saves", "moral", "treasure", "alignment", "xp_value"]
-        for stat in stats:
-            value[stat] = ""
-        
-        with open("/resources/cfg/monster.cfg", "r") as monster_config:
-            for line in monster_config:
-                pass
-        return return_dict
+    """
+    Method to return a dict of all the available monsters. Reads the resource/cfg/monsters.cfg file to fill the dict.
+
+    Formatted entry in monsters.cfg:
+    monster_name{
+    environment_name\n
+    life\n
+    ac\n
+    movement\n
+    attacks\n
+    damages\n
+    number_met\n
+    saves\n
+    moral\n
+    treasure\n
+    alignment\n
+    xp_value\n
+    }
+    """
+    monster_dict={}
+    name=""
+    value={}        
+
+    stats=["environment","life", "ac", "movement", "attacks", "damages", "number_met", "saves", "moral", "treasure", "alignment", "xp_value"]
+    for stat in stats:
+        value[stat] = ""
+    i = 0
+
+    with open("/resources/cfg/monster.cfg", "r") as monster_config:
+        for line in monster_config:
+            if "{" in line:
+                name = line.strip("{")
+            elif "}" in line:
+                monster_dict[name]=value
+                i = 0
+                for stat in stats:
+                    value[stat] = ""
+                name = ""
+            else:
+                line.strip("\n")
+                value[stats[i]] = line
+                i += 1     
+    return monster_dict
+
+def get_random_encounters_table(monster_dict, environment):
+    list_encounters=[""]*12
+    monsters_that_can_be_picked=[]
+    for name, stats in monster_dict.items():
+        if stats["environment"] == environment:
+            monsters_that_can_be_picked.append(name)
+    for i in range(12):
+        list_encounters[i] = rd.choice(monsters_that_can_be_picked)
+    return list_encounters
+
+def get_a_monster(list_encounters, chance_encounter):
+    dice = rd.randint(1,12)
+    chance = rd.randint(1,100)
+    if chance <= chance_encounter:
+        return list_encounters[dice]
+    else:
+        return "No Monster Met"
+
+def get_a_monster_stats(monster_dict, name):
+    #stats=["environment","life", "ac", "movement", "attacks", "damages",\
+    #"number_met", "saves", "moral", "treasure", "alignment", "xp_value"]
+    try:
+        assert name != "No Monster Met", "no monster met, no stat to retrieve"
+    except AssertionError:
+        return ("_")*15
+    stats_of_monster = monster_dict[name]
+    
+    #Getting the life points of monster : life => 3d6+-1 => (3 x 6-sided dices) minus 1
+    life = stats_of_monster["life"]
+    number_of_dices = int(life.split("d")[0])
+    sides_of_dice = int(life.split("d")[1].split("+")[0])
+    modifier_of_dice = int(life.split("d")[1].split("+")[1])
+    temp=[]
+    for i in range(number_of_dices):
+        temp.append(rd.randint(1,sides_of_dice))
+    life=sum(temp)+modifier_of_dice
+    
+    #Getting the Armor Class (AC)
+    ac = stats_of_monster["ac"]
+    
+    #Getting the movement value
+    movement=stats_of_monster["movement"]
+    
+    return ("_")*15
