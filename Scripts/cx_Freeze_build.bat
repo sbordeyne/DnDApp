96e3ef6be27d347448bc3d6ee02bef24928515e2
Dogeek <simon.bordeyne@gmail.com>
2015-08-15 08:33:33 +0200

    built software

--- Scripts/cx_Freeze_build.bat
+++ Scripts/cx_Freeze_build.bat
@@ -1,2 +1,10 @@
 cd..
 python setup.py build
+pause
+del ../build/exe.win32-3.4/resources/cfg/*.* /Q
+del ../build/exe.win32-3.4/resources/sound/*.* /Q
+del ../build/exe.win32-3.4/resources/fonts/*.* /Q
+robocopy ../resources/cfg ../build/exe.win32-3.4/resources/cfg /E
+robocopy ../resources/sound ../build/exe.win32-3.4/resources/sound /E
+robocopy ../resources/fonts ../build/exe.win32-3.4/resources/fonts /E
+pause
